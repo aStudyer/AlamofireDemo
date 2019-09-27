cf8bce20a7b480d0b7e6bf7280f30d83a60b6141
wxGitHup <718753218@qq.com>
2019-09-27 15:52:30 +0800

    init

---
+++ AlamofireDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireDemo
+//
+//  Created by 王翔 on 2019/9/27.
+//  Copyright © 2019 aStudyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
